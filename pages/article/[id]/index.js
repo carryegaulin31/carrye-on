77367a89352b87d56dfa3e17898cc222eaddae1a
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-08-05 12:22:14 -0400

    did a fetch with getServerSideProps and made it so that it returned the title, body and created a GoBack option by importing Link

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,10 +1,30 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useRouter } from "next/router";
+import Link from "next/link";
 
-const article = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+const article = ({ article }) => {
+  // const router = useRouter()
+  // const {id} = router.query and it shows you which article number it is also would need This is an article {id} in the div
+  return <>
+  <h1>{article.title}</h1>
+  <p>{article.body}</p>
+  <br />
+  <Link href='/'>Go Back</Link>
+  </>;
+};
 
-export default article
+// getServerSideProps which will fetch on request...context allows to get the id from the url
+export const getServerSideProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+
+  const article = await res.json();
+
+  return {
+    props: {
+      article,
+    },
+  };
+};
+export default article;
