3de66779507068267785738f9c703318af3ddffe
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-08-05 10:57:11 -0400

    added Nav style file and Header style file added a Header component with some jsx style IN the code as an example

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Nav from './Nav'
 import styles from "../styles/Layout.module.css";
 
 const Layout = ({ children }) => {
     return (
+    <>
+    <Nav />
         <div className={styles.container}>
             <main className={styles.main}>{children}</main>
         </div>
+        </>
     );
 };
 
--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-
+import Link from 'next/link'
 const about = () => {
   return (
     <div>
