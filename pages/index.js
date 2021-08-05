0afe90f9fe4f120764973e51bc36fdc0cf2989bd
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-08-05 10:36:02 -0400

    Created Layout component and wrapped the page with it in the _app.js file

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,12 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div >
-      
+    <div>
+       <Head>
+       <title>Carrye On</title>
+       <meta name='keywords' content='blog, technical writing' />
+       </Head>
+       <h1>Welcome to my Blog</h1>
     </div>
   )
 }
