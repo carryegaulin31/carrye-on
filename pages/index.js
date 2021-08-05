38507c81e4c5185cd81d838044a769801ac7b5dc
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-08-05 11:50:54 -0400

    added an async call to a public API and limited it to 6 and asked for the articles to be what was returned and console logged to be sure that they actually came back and they did

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-
-export default function Home() {
+export default function Home({ articles }) {
+console.log(articles)
   return (
     <div>
-       <Head>
-       <title>Carrye On</title>
-       <meta name='keywords' content='blog, technical writing' />
-       </Head>
-       <h1>Welcome to my Blog</h1>
+      <Head>
+        <title>Carrye On</title>
+        <meta name="keywords" content="blog, technical writing" />
+      </Head>
+      <h1>Welcome to my Blog</h1>
     </div>
-  )
+  );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  );
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
